38ad1915580aa7b74e1d6931656ce21a7a443200
Aniketh <89690272+AbsoluteReality@users.noreply.github.com>
2024-12-31 13:35:18 +0530

    dashboard table populated with GET requests

--- app/components/AppSidebar.tsx
+++ app/components/AppSidebar.tsx
@@ -1,44 +1,51 @@
-'use client';
+"use client";
 
-import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import Link from 'next/link';
-import { ModeToggle } from '@/components/mode-toggle';
-import { Inbox } from 'lucide-react';
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Inbox } from "lucide-react";
 
 const items = [
-	{
-		title: 'Dashboard',
-		url: '/',
-		icon: Inbox,
-	},
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: Inbox,
+  },
 ];
 
 const AppSidebar = () => {
-	return (
-		<>
-			<div className="p-1">
-				<ModeToggle />
-			</div>
-			<div className="text-3xl font-bold px-2">Chaya </div>
-			<SidebarGroup>
-				<SidebarGroupLabel>Menu</SidebarGroupLabel>
-				<SidebarGroupContent>
-					<SidebarMenu>
-						{items.map((item) => (
-							<SidebarMenuItem key={item.title}>
-								<SidebarMenuButton asChild>
-									<Link href={item.url}>
-										<item.icon />
-										{item.title}
-									</Link>
-								</SidebarMenuButton>
-							</SidebarMenuItem>
-						))}
-					</SidebarMenu>
-				</SidebarGroupContent>
-			</SidebarGroup>
-		</>
-	);
+  return (
+    <>
+      <div className="p-1">
+        <ModeToggle />
+      </div>
+      <div className="text-3xl font-bold px-2 text-slate-950">Chaya </div>
+      <SidebarGroup>
+        <SidebarGroupLabel>Menu</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map(item => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <Link href={item.url}>
+                    <item.icon />
+                    {item.title}
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </>
+  );
 };
 
 export default AppSidebar;
